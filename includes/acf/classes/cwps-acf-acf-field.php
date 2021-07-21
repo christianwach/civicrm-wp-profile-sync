35a7f2fc87fefde298f299177229145d713fdada
Christian Wach <needle@haystack.co.uk>
2021-07-21 09:23:17 +0100

    Fix missing linkage on ACF select fields

--- includes/acf/classes/cwps-acf-acf-field.php
+++ includes/acf/classes/cwps-acf-acf-field.php
@@ -710,7 +710,7 @@ class CiviCRM_Profile_Sync_ACF_Field {
 		if ( ! empty( $participant_fields ) ) {
 			$setting = $this->acf_loader->civicrm->participant->acf_field_get( $filtered_fields, $participant_fields );
 		}
-		if ( ! empty( $activity_fields ) AND ! empty( $participant_fields ) ) {
+		if ( empty( $activity_fields ) AND empty( $participant_fields ) ) {
 			$setting = $this->acf_loader->civicrm->contact->acf_field_get( $filtered_fields, $contact_fields );
 		}
 
