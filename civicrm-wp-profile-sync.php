51cde37d1f02356ab4acaa708c2808dfabd69a10
Christian Wach <needle@haystack.co.uk>
2024-10-17 11:18:22 +0100

    Update plugin header

--- civicrm-wp-profile-sync.php
+++ civicrm-wp-profile-sync.php
@@ -9,11 +9,26 @@
  * Version:           0.6.9a
  * Author:            Christian Wach
  * Author URI:        https://haystack.co.uk
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Requires at least: 5.7
+ * Requires PHP:      7.4
  * Text Domain:       civicrm-wp-profile-sync
  * Domain Path:       /languages
  *
  * @package CiviCRM_WP_Profile_Sync
- * @since 0.1
+ * @link    https://github.com/christianwach/civicrm-wp-profile-sync
+ * @license GPL v2 or later
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 // Exit if accessed directly.
