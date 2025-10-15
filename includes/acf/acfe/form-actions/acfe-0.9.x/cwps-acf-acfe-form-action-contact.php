09ad17c678a2a739dba7f107735aca2ea4d63684
Christian Wach <needle@haystack.co.uk>
2025-10-15 14:47:58 +0100

    Prepare to enable address data when deduping

--- includes/acf/acfe/form-actions/acfe-0.9.x/cwps-acf-acfe-form-action-contact.php
+++ includes/acf/acfe/form-actions/acfe-0.9.x/cwps-acf-acfe-form-action-contact.php
@@ -1263,6 +1263,9 @@ class CWPS_ACF_ACFE_Form_Action_Contact extends CWPS_ACF_ACFE_Form_Action_Base {
 		// Populate Email data array.
 		$emails = $this->form_email_data( $form, $action );
 
+		// Populate Address data array.
+		$addresses = $this->form_address_data( $form, $action );
+
 		// Build Contact Custom Field args.
 		$args = [
 			'custom_groups' => $this->custom_fields,
@@ -1272,7 +1275,7 @@ class CWPS_ACF_ACFE_Form_Action_Contact extends CWPS_ACF_ACFE_Form_Action_Base {
 		$contact_custom_fields = $this->form_entity_custom_fields_data( $action['contact'], $args );
 
 		// Get the Contact ID with the data from the Form.
-		$contact_id = $this->form_contact_id_get( $contact, $emails, $action );
+		$contact_id = $this->form_contact_id_get( $contact, $emails, $addresses, $action );
 
 		// Parse Relationships.
 		$relationship_data = [];
@@ -1320,7 +1323,6 @@ class CWPS_ACF_ACFE_Form_Action_Contact extends CWPS_ACF_ACFE_Form_Action_Base {
 			}
 
 			// Save the Address(es) with the data from the Form.
-			$addresses         = $this->form_address_data( $form, $action );
 			$result['address'] = $this->form_address_save( $result['contact'], $addresses );
 
 			// Save the Website(s) with the data from the Form.
@@ -4175,8 +4177,11 @@ class CWPS_ACF_ACFE_Form_Action_Contact extends CWPS_ACF_ACFE_Form_Action_Base {
 		// Get the Emails.
 		$emails = $this->form_email_data( $form, $action );
 
+		// Get the Addresses.
+		$addresses = $this->form_address_data( $form, $action );
+
 		// Get the Contact ID with the data from the Form.
-		$contact_id = $this->form_contact_id_get( $contact, $emails, $action );
+		$contact_id = $this->form_contact_id_get( $contact, $emails, $addresses, $action );
 
 		// All's well if we get a Contact ID.
 		if ( ! empty( $contact_id ) ) {
@@ -4407,10 +4412,11 @@ class CWPS_ACF_ACFE_Form_Action_Contact extends CWPS_ACF_ACFE_Form_Action_Base {
 	 *
 	 * @param array $contact_data The array of Contact data.
 	 * @param array $email_data The array of Email data.
+	 * @param array $address_data The array of Address data.
 	 * @param array $action The array of Action data.
 	 * @return integer|bool $contact_id The numeric ID of the Contact, or false if not found.
 	 */
-	private function form_contact_id_get( $contact_data, $email_data, $action ) {
+	private function form_contact_id_get( $contact_data, $email_data, $address_data, $action ) {
 
 		// Init return.
 		$contact_id = false;
@@ -4422,7 +4428,7 @@ class CWPS_ACF_ACFE_Form_Action_Contact extends CWPS_ACF_ACFE_Form_Action_Base {
 		}
 
 		// Try the "Deduped Contact".
-		$deduped = $this->form_contact_id_get_deduped( $contact_data, $email_data, $action );
+		$deduped = $this->form_contact_id_get_deduped( $contact_data, $email_data, $address_data, $action );
 		if ( $deduped ) {
 			return $deduped;
 		}
@@ -4607,10 +4613,11 @@ class CWPS_ACF_ACFE_Form_Action_Contact extends CWPS_ACF_ACFE_Form_Action_Base {
 	 *
 	 * @param array $contact_data The array of Contact data.
 	 * @param array $email_data The array of Email data.
+	 * @param array $address_data The array of Address data.
 	 * @param array $action The array of Action data.
 	 * @return integer|bool $contact_id The numeric ID of the Contact, or false if not found.
 	 */
-	private function form_contact_id_get_deduped( $contact_data, $email_data, $action ) {
+	private function form_contact_id_get_deduped( $contact_data, $email_data, $address_data, $action ) {
 
 		// Init return.
 		$contact_id = false;
